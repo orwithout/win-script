a0a6b570bcaf10107c1ecb97d2b3a15c79e4614a
orwithout <he.haifeng@hotmail.com>
2021-07-05 18:25:06 +0800

    add

--- windows激活/win2019数据中心-激活.cmd
+++ windows激活/win2019数据中心-激活.cmd
@@ -1,3 +1,19 @@
+@echo off
+cd /d "%~dp0"
+cacls.exe "%SystemDrive%\System Volume Information" >nul 2>nul
+if %errorlevel%==0 goto Admin
+if exist "%temp%\getadmin.vbs" del /f /q "%temp%\getadmin.vbs"
+echo Set RequestUAC = CreateObject^("Shell.Application"^)>"%temp%\getadmin.vbs"
+echo RequestUAC.ShellExecute "%~s0","","","runas",1 >>"%temp%\getadmin.vbs"
+echo WScript.Quit >>"%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs" /f
+if exist "%temp%\getadmin.vbs" del /f /q "%temp%\getadmin.vbs"
+exit
+
+:Admin
+
+
+
 slmgr.vbs -upk && slmgr /ipk WMDGN-G9PQG-XVVXX-R3X43-63DFG && slmgr /skms zh.us.to && slmgr /ato && slmgr /xpr
 
 
--- windows激活/win2019标准版-激活.cmd
+++ windows激活/win2019标准版-激活.cmd
@@ -1,3 +1,19 @@
+@echo off
+cd /d "%~dp0"
+cacls.exe "%SystemDrive%\System Volume Information" >nul 2>nul
+if %errorlevel%==0 goto Admin
+if exist "%temp%\getadmin.vbs" del /f /q "%temp%\getadmin.vbs"
+echo Set RequestUAC = CreateObject^("Shell.Application"^)>"%temp%\getadmin.vbs"
+echo RequestUAC.ShellExecute "%~s0","","","runas",1 >>"%temp%\getadmin.vbs"
+echo WScript.Quit >>"%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs" /f
+if exist "%temp%\getadmin.vbs" del /f /q "%temp%\getadmin.vbs"
+exit
+
+:Admin
+
+
+
 slmgr.vbs -upk && slmgr.vbs -ipk N69G4-B89J2-4G8F4-WWYCC-J464C && slmgr /skms zh.us.to && slmgr /ato && slmgr /xpr
 
 
